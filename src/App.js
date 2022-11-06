3a48e58dcc661295d5c9806fa5bf7c303f5c2f5a
oscarluna <oscarluna.webdev@gmail.com>
2022-11-06 14:11:42 -0800

    Created Firebase.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [cometChat, setCometChat] = useState({})
+
+  useEffect(() => {
+    initCometChat();
+  }, []);
+  
+  /**
+   * init comet chat.
+   */
+   const initCometChat = async () => {
+    const { CometChat } = await import('@cometchat-pro/chat');
+    const appID = `${process.env.REACT_APP_COMETCHAT_APP_ID}`;
+    const region = `${process.env.REACT_APP_COMETCHAT_REGION}`;
+    const appSetting = new CometChat.AppSettingsBuilder().subscribePresenceForAllUsers().setRegion(region).build();
+    CometChat.init(appID, appSetting).then(
+      () => {
+        setCometChat(() => CometChat);
+      },
+      error => {
+      }
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
